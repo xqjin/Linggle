1357fdc2e5a3be0415f4eee09b92a662df4f5e66
xqjin <jinxiaoqiang0608@gmail.com>
2015-04-18 20:23:56 +0800

    modify

--- parseLinggle.py
+++ parseLinggle.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
-import random
-import pickle
-import re
-import sys
-import os
 import urllib
 import json
-import configure
-import parseNumTree as pnt
-from stemming.porter2 import stem
-from Key2Value import PrepKV as PKV
-from Key2Value import ArtKV as AKV
-from Key2Value import NnKV as NKV
-from nltk.corpus import cmudict as cmu
-from nltk.corpus import wordnet as wn
-from Arpabet import isAorAn
-from UCN import UCN
-import nltk
+
 
 
 def parselinggle(itemlist):
-    #token = "%20".join(itemlist)
+    #itemlist : a list of words
     token = " ".join(itemlist)
     # insert sapce or not is the same!
     page = urllib.urlopen(u'http://www.linggle.com/query/%s' %token)
