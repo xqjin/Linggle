6ed37d54a428531eec5d55cec760ebfb5a862039
xqjin <xqjin@Novice.(none)>
2015-04-20 09:45:20 +0800

    modify

--- parseLinggle.py
+++ parseLinggle.py
@@ -9,7 +9,7 @@ def parselinggle(itemlist):
     #itemlist : a list of words
     token = " ".join(itemlist)
     # insert sapce or not is the same!
-    page = urllib.urlopen(u'http://www.linggle.com/query/%s' %token)
+    page = urllib.urlopen(u'http://www.linggle.com/query/%s' %token,timeout=1000)
     if page.getcode()==404:
         return 0
 
