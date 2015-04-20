d4ccfe918c7b80fb7ee91d082d47409e444373bc
xqjin <xqjin@Novice.(none)>
2015-04-20 12:00:23 +0800

    commit

--- parseLinggle.py
+++ parseLinggle.py
@@ -2,19 +2,24 @@
 #-*- coding:utf-8 -*-
 import urllib2
 import json
-
+import re
 
 
 def parselinggle(itemlist):
     #itemlist : a list of words
-    token = " ".join(itemlist)
+    itemlist = [v for v in itemlist if v]
+    #you'd better use the %20 to fill the blank between the word or it may occur an error!
+    token = "%20".join(itemlist)
     # insert sapce or not is the same!
     page = urllib2.urlopen(u'http://www.linggle.com/query/%s' %token,timeout=1000)
-    if page.getcode()==404:
+    if page.getcode()==200:
+        result = json.loads(page.read())
+        if not result:
+            return 0
+        else:
+            return result[0]["count"]
+    else:
         return 0
 
-    result = json.loads(page.read())
-    if not result:
-        return 0
-    else:
-        return result[0]["count"]
+if __name__ == "__main__":
+    parselinggle(['resource','HR'])
